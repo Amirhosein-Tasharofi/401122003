a3c81206c11a4fcffc29f7e8db2f611e33b79cfa
Amirhosein-Tasharofi <amirhosein.tasharofi82@gmail.com>
2025-05-28 00:23:17 +0330

    پیام کامیت شما

--- app/Controller/FrontController.php
+++ app/Controller/FrontController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 use App\Model\User;
 class FrontController{
     public static function home(){
-        // $id = $_GET['id'];
-        // $user  = User::find($id);
-        // var_dump($user->composite());
+        $id = $_GET['id'];
+        $user  = User::find($id);
+        var_dump($user->composite());
         return view('home.php');
     }
     public static function about(){
--- index.php
+++ index.php
@@ -5,6 +5,19 @@ require_once(__DIR__.'/helper/functions.php');
 
 use App\Route;
 use App\Controller\FrontController;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$config = require __DIR__.'/config/database.php';
+$capsule = new Capsule;
+$capsule->addConnection($config);
+// Make this Capsule instance available globally via static methods
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM
+$capsule->bootEloquent();
+
+// $users = Capsule::table('users')->where('id', '>', 1)->get();
+// var_dump($users);
+// exit();
 
 $route = new Route();
 $route->addRoute("GET","/WebProgramming/",[FrontController::class, 'home']);
@@ -12,36 +25,4 @@ $route->addRoute("GET",'/WebProgramming/about',[FrontController::class, 'about']
 $route->addRoute("GET",'/WebProgramming/infs',[FrontController::class, 'infs']);
 $route->dispatch();
 
-// $method = $_SERVER['REQUEST_METHOD'];
-// $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
-
-
-// // echo($method);
-// // echo('<br>');
-// // print_r($uri);
-
-// if($uri == "/WebProgramming/")
-// {
-//     view('home.php');
-
-// }
-// else if ($uri == '/WebProgramming/infs')
-// {
-//     view('infs.php');
-
-// }
-// else if($uri == '/WebProgramming/header')
-// {
-//     view('header.php');
-
-// }else if($uri == '/WebProgramming/about')
-// {
-//     view('about.php');
-
-// }
-// else{
-//     view('404.php');
-    
-// }
-
-// ?>
+?>
